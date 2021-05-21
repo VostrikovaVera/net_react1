2610c6d6e2ff2c9aa86ab17aa64477f5653ea9b1
Vira Vostrikova <vvostrikova@csod.com>
2021-05-21 14:43:53 +0300

    Tasks 1 & 2 & 3 done without markup

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,59 @@ import { UserCard } from "./components/user-card/UserCard";
 
 function App() {
   const [userData, setUserData] = useState(rawUserData);
+  const [currentFilterValue, setCurrentFilterValue] = useState('');
+  const [currentSortType, setCurrentSortType] = useState('');
 
   const handleFilterChange = (e) => {
-    const filteredData = rawUserData.filter(user => {
-      if (user.name.toLowerCase().includes(e.target.value.toLowerCase())) {
+    const filterValue = e.target.value;
+    setCurrentFilterValue(filterValue);
+
+    const filteredData = onDataFilter(rawUserData, filterValue);
+    const filteredSortedData = onDataSort(filteredData, currentSortType);
+
+    setUserData(filteredSortedData);
+  }
+
+  const handleSortChange = (e) => {
+    const sortType = e.target.value;
+    setCurrentSortType(sortType);
+
+    const sortedData = sortType === '' ?
+        onDataFilter(rawUserData, currentFilterValue) :
+        onDataSort([...userData], sortType);
+
+    setUserData(sortedData)
+  }
+
+  const handleReset = () => {
+    setCurrentSortType('');
+    setCurrentFilterValue('');
+    setUserData(rawUserData);
+  }
+
+  const onDataFilter = (data, filterValue) => {
+    return data.filter(user => {
+      if (user.name.toLowerCase().includes(filterValue.toLowerCase())) {
         return user;
       }
     })
-
-    // call sort
-
-    setUserData(filteredData);
   }
 
-  const handleSort = (e) => {
-    const sortType = e.target.value;
-
-    const sortedData = [...userData]
-        .sort((a,b) => sortType === 'asc' ? a.age - b.age : b.age - a.age)
-
-    // call filter
-
-    setUserData(sortedData)
+  const onDataSort = (data, sortType) => {
+    return data.sort((a,b) => sortType === 'asc' ? a.age - b.age : b.age - a.age);
   }
 
   return (
     <div className="App">
       <div className="App-container">
         <header className="App-header">
-          <input type="text" placeholder="Search by user name" onChange={handleFilterChange} />
-          <select onChange={handleSort}>
+          <input type="text" placeholder="Search by user name" value={currentFilterValue} onChange={handleFilterChange}/>
+          <select onChange={handleSortChange} value={currentSortType}>
             <option value="">--Please select--</option>
             <option value="asc">Asc</option>
             <option value="desc">Desc</option>
           </select>
+          <button onClick={handleReset}>Reset All</button>
         </header>
 
         <div className="App-users-container">
